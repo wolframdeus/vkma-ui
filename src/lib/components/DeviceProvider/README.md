9165ca4bde6efee02e9c6d86962046cc92c58d6d
Vladislav Kibenko <wolfram.deus@gmail.com>
2020-05-28 04:28:53 +0500

    fix(getinsets): fixed getInsets

--- src/lib/components/DeviceProvider/README.md
+++ src/lib/components/DeviceProvider/README.md
@@ -5,15 +5,3 @@ is being shared:
 
 - Screen current and initial insets
 - Operating system
-
-To make `getInsets` work, add this style to head of your index.html:
-```css
-<style>
-    :root {
-        --safe-area-inset-top: env(safe-area-inset-top, 0px);
-        --safe-area-inset-right: env(safe-area-inset-right, 0px);
-        --safe-area-inset-bottom: env(safe-area-inset-bottom, 0px);
-        --safe-area-inset-left: env(safe-area-inset-left, 0px);
-    }
-</style>
-```
--- src/lib/components/DeviceProvider/utils.ts
+++ src/lib/components/DeviceProvider/utils.ts
@@ -14,14 +14,32 @@ export function getOS(userAgent: string): OS {
  * @returns {Insets}
  */
 export function getInsets(): Insets {
+  // Append style element tog get current insets via env variables
+  const style = document.createElement('style');
+  style.innerHTML = `
+  :root {
+    --safe-area-inset-top: env(safe-area-inset-top, 0px);
+    --safe-area-inset-right: env(safe-area-inset-right, 0px);
+    --safe-area-inset-bottom: env(safe-area-inset-bottom, 0px);
+    --safe-area-inset-left: env(safe-area-inset-left, 0px);
+  }`;
+
+  // Append to header
+  document.head.appendChild(style);
+
   // Compute style
   const computedStyle = getComputedStyle(document.documentElement);
 
   // Derive insets
-  return {
+  const insets = {
     top: parseInt(computedStyle.getPropertyValue('--safe-area-inset-top')),
     bottom: parseInt(computedStyle.getPropertyValue('--safe-area-inset-bottom')),
     left: parseInt(computedStyle.getPropertyValue('--safe-area-inset-left')),
     right: parseInt(computedStyle.getPropertyValue('--safe-area-inset-right')),
   };
+
+  // Cleanup
+  document.head.removeChild(style);
+
+  return insets;
 }
